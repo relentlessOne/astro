2f5e48925620cb9d7f75824e0a74d4da16185554
pai-55 <pai-55@sr.dmcs.pl>
2017-06-13 21:07:53 +0200

    upp

--- app/src/main/java/com/example/pc/astro/MainActivity.java
+++ app/src/main/java/com/example/pc/astro/MainActivity.java
@@ -1,45 +1,223 @@
 package com.example.pc.astro;
 
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Environment;
+import android.os.Message;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.format.DateFormat;
+import android.util.Xml;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.astrocalculator.AstroCalculator;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+import org.xmlpull.v1.XmlSerializer;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ButtonFragment.ButtonFragmentClicksListener {
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity implements ButtonFragment.ButtonFragmentClicksListener, WeatherMenuFragment.WeatherMenuListener {
 
     TimeLocalizationFragment timeLocalization;
     SunFragment sun;
     MoonFragment moon;
-
+    WeatherMenuFragment wmf;
+    Context context;
+    boolean lastModal;
+    boolean allowInit;
+    String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/astroData";
     String m_Text = "";
     String m_Text1 = "";
+    String choosenCity ;
+    String choosenWOEID;
     double lat = 51.759248;
     double lng = 19.455983;
     long time = 1000;
+    JSONArray favJSON;
+
+    File rootDir;
+    File favXML;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        rootDir = new File(path);
+
+
+
+
+        context = this;
+        allowInit = true;
         setContentView(R.layout.main);
         loadElements();
         startClock();
         refreshSunMoonData();
+
+        checkNetConnection();
     }
+
+    private void checkNetConnection(){
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000);
+
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if(isConnected()){
+                                    if(!lastModal || allowInit){
+                                        Toast.makeText(context, "Connected to web",
+                                                Toast.LENGTH_LONG).show();
+
+                                        loadFav();
+
+                                        lastModal = true;
+                                        allowInit = false;
+                                    }
+                                }else {
+                                    if(lastModal || allowInit){
+                                        Toast.makeText(context, "No internet connection",
+                                                Toast.LENGTH_LONG).show();
+                                        lastModal = false;
+                                        allowInit = false;
+                                    }
+                                }
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+    }
+
+
+
+    private void loadFav(){
+        deleteFile("fav.json");
+        checkIfFileExists("fav.json");
+     //   expandJson("fav.json","xzzzx","xx");
+        wmf.cityName.setText(readFromFile("fav.json"));
+
+
+           // overwriteFile("fav.json","asdasdasd");
+            //expandJson("fav.json","Warszawa","213123");
+           // wmf.cityName.setText(readFromFile("fav.json"));
+
+
+
+    }
+
+
+    private void checkIfFileExists(String filename){
+        try {
+            FileInputStream fin = openFileInput(filename);
+
+        } catch (FileNotFoundException e) {
+            FileOutputStream fos = null;
+            try {
+                JSONArray initialFav = new JSONArray();
+                JSONObject tmp = new JSONObject();
+                tmp.put("Lodz","505120");
+                initialFav.put(tmp);
+                fos = openFileOutput(filename,MODE_PRIVATE);
+                fos.write(initialFav.toString().getBytes());
+                fos.close();
+
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+
+        }
+    }
+
+    private void expandJson(String filename,String key, String value){
+        try {
+            JSONArray jsonarr = new JSONArray(readFromFile(filename));
+            JSONObject json = new JSONObject();
+            json.put(key,value);
+            jsonarr.put(json);
+            overwriteFile(filename,jsonarr.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void overwriteFile(String filename,String msg){
+        deleteFile(filename);
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(filename,MODE_PRIVATE);
+            fos.write(msg.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private String readFromFile(String filename){
+        try {
+            String msg;
+            FileInputStream fin = openFileInput(filename);
+            InputStreamReader isr = new InputStreamReader(fin);
+            BufferedReader bis = new BufferedReader(isr);
+            StringBuffer sb = new StringBuffer();
+            while((msg = bis.readLine())!=null){
+                sb.append(msg + '\n');
+            }
+            favJSON = new JSONArray(sb.toString());
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "noFile";
+
+        }
+    }
+
 
     private void startClock() {
         Thread t = new Thread() {
@@ -72,6 +250,9 @@ public class MainActivity extends AppCompatActivity implements ButtonFragment.Bu
         timeLocalization.localization.setText(lat + " " + lng);
         sun = (SunFragment) getSupportFragmentManager().findFragmentById(R.id.sun);
         moon = (MoonFragment) getSupportFragmentManager().findFragmentById(R.id.moon);
+        wmf = (WeatherMenuFragment) getSupportFragmentManager().findFragmentById(R.id.weatherMenu);
+        wmf.cityName.setText("Łodź");
+
     }
 
 
@@ -125,7 +306,7 @@ public class MainActivity extends AppCompatActivity implements ButtonFragment.Bu
         builder.setTitle("");
 
         final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
         builder.setView(input).setMessage("Odświeżanie w minutach");
 
 
@@ -196,10 +377,10 @@ public class MainActivity extends AppCompatActivity implements ButtonFragment.Bu
         builder.setTitle("");
 
         final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
 
         final EditText input1 = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
 
 
 
@@ -227,4 +408,188 @@ public class MainActivity extends AppCompatActivity implements ButtonFragment.Bu
 
         builder.show();
     }
+
+    private boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isAvailable()
+                && cm.getActiveNetworkInfo().isConnected()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void refreshWeatherClick() {
+
+    }
+
+    private boolean validateInputNewCity(String msg){
+        final String msgCpy = msg;
+        List<String> listdata = new ArrayList<String>();
+        JSONArray jArray = favJSON;
+        if (jArray != null) {
+            for (int i=0;i<jArray.length();i++){
+                try {
+                    listdata.add(jArray.getString(i));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        for(String el : listdata){
+            try {
+                JSONObject tmp = new JSONObject(el);
+                Iterator<?> keys = tmp.keys();
+                final String key = (String)keys.next();
+                if(key.equals(msg)){
+                    Toast.makeText(this, "Istnieje miasto "+msg+" w ulubionych",
+                            Toast.LENGTH_LONG).show();
+                    return  false;
+                } else {
+
+                    String url = "https://query.yahooapis.com/v1/public/yql?q=select * from geo.places(1) where text=\""+msg+"\"&format=json";
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    client.get(url, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            if (responseBody == null) { /* empty response, alert something*/ return; }
+                            //success response, do something with it!
+                            String response = new String(responseBody);
+
+                            try {
+                                JSONObject resjson = new JSONObject(response);
+
+                                JSONObject query = resjson.getJSONObject("query");
+
+                                String count = query.getString("count");
+                                if(!count.equals("0")){
+                                    JSONObject results = query.getJSONObject("results");
+                                    JSONObject place = results.getJSONObject("place");
+                                    String placename = place.getString("name");
+                                    String woeid = place.getString("woeid");
+                                    expandJson("fav.json",placename,woeid);
+                                    Toast.makeText(context, placename+" zostal dodany",Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(context, "Nie znaleziono miasta "+msgCpy,Toast.LENGTH_LONG).show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                            if (responseBody == null) { /* empty response, alert something*/ return; }
+                            //error response, do something with it!
+                            String response = new String(responseBody);
+                        }
+                    });
+
+                    return true;
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return true;
+    };
+
+    @Override
+    public void cityWeatherClick() {
+        m_Text = "";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
+        builder.setView(input).setMessage("Nazwa miasta");
+
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+                validateInputNewCity(m_Text);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    public void favWeatherClick() {
+        readFromFile("fav.json");
+        List<String> listdata = new ArrayList<String>();
+        JSONArray jArray = favJSON;
+        if (jArray != null) {
+            for (int i=0;i<jArray.length();i++){
+                try {
+                    listdata.add(jArray.getString(i));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("");
+        LinearLayout ll=new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+
+        for(String el : listdata){
+            Button btnTag = new Button(this);
+
+            try {
+                final JSONObject json = new JSONObject(el);
+                Iterator<?> keys = json.keys();
+                final String key = (String)keys.next();
+                btnTag.setText(key);
+                btnTag.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                       choosenCity = key;
+                        try {
+                            choosenWOEID = json.getString(key);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                ll.addView(btnTag);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        ScrollView sv = new ScrollView(this);
+        sv.addView(ll);
+        builder.setView(sv);
+
+
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/pc/astro/WeatherMenuFragment.java
@@ -0,0 +1,85 @@
+package com.example.pc.astro;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class WeatherMenuFragment extends Fragment {
+    public Button refresh;
+    public Button city;
+    public Button fav;
+    public TextView cityName;
+
+    WeatherMenuListener WeatherMenuCommander;
+
+    public interface WeatherMenuListener{
+        public void refreshWeatherClick();
+        public void cityWeatherClick();
+        public void favWeatherClick();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            WeatherMenuCommander = (WeatherMenuFragment.WeatherMenuListener) context;
+        } catch (Exception e){
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.weather_menu,container,false);
+        fav = (Button) view.findViewById(R.id.fav);
+        refresh = (Button) view.findViewById(R.id.refresh);
+        city = (Button) view.findViewById(R.id.city);
+        cityName = (TextView) view.findViewById(R.id.cityName);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                WeatherMenuCommander.refreshWeatherClick();
+            }
+        });
+
+        city.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                WeatherMenuCommander.cityWeatherClick();
+            }
+        });
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                WeatherMenuCommander.refreshWeatherClick();
+            }
+        });
+
+        fav.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                WeatherMenuCommander.favWeatherClick();
+            }
+        });
+
+        return view;
+    }
+
+
+
+
+
+
+
+
+}
